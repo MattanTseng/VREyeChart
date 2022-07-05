70cc31c4fe2d3ab112e8adb1599061004c4245fa
MattanTseng <mstseng@iastate.edu>
2022-07-05 14:41:29 -0500

    Started something... got distracted

---
+++ Assets/Scripts/PresetPublisher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+
+
+public class PresetPublisher : MonoBehaviour
+{
+
+    public GameObject Background;
+    public TMP_FontAsset Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NewChanges()
+    {
+        Text.
+    }
+}
